4ee6f7a5424be1722f2655e6e557f7e0b6173f65
youngpark76 <yjp269@nyu.edu>
2025-06-17 23:18:51 -0400

    Fixed tooltip

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function () {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
+});
